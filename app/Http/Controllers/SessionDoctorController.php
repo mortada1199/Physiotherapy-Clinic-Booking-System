f64563c49916c3ffe008eb1166eddfb8c17a403f
mortada1199 <murtada199815@gmail.com>
2025-11-03 09:02:43 +0200

    add sesion

--- app/Http/Controllers/SessionDoctorController.php
+++ app/Http/Controllers/SessionDoctorController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class SessionController extends Controller
+class SessionDoctorController extends Controller
 {
     //
 }
--- database/migrations/2025_11_03_070155_create_session_doctors_table.php
+++ database/migrations/2025_11_03_070155_create_session_doctors_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sessions', function (Blueprint $table) {
+        Schema::create('session_doctors', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('session_doctors');
     }
 };
