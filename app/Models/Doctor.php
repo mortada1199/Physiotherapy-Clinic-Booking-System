046e76b337208518178476fab617c9d75ecd3808
mortada1199 <murtada199815@gmail.com>
2025-11-03 09:31:06 +0200

    add fillabe to doctor

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     //
+    protected $fillable = [
+        'name',
+        'experience',
+        'address',
+        'phone',
+        'persent',
+        'email',
+    ];
+
 }
--- database/migrations/2025_11_03_065631_create_doctors_table.php
+++ database/migrations/2025_11_03_065631_create_doctors_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('experience');
+            $table->string('address');
+            $table->string('phone')->unique();
+            $table->string('persent');
+            $table->string('email')->unique();  
             $table->timestamps();
         });
     }
