7a076ecbdbdb1418829020856e7f67f57079b22c
mortada1199 <murtada199815@gmail.com>
2025-11-23 12:36:05 +0200

    add

--- resources/views/layouts/dashbord.blade.php
+++ resources/views/layouts/dashbord.blade.php
@@ -152,7 +152,7 @@
                         <i class="la la-wheelchair"></i>
                         <span class="menu-title">المرضى</span>
                         <span class="badge badge-info badge-pill float-right mr-2">
-                            {{ App\Models\Patient::select('patientnumber', 'name')->distinct()->count() ?? 0 }}
+                            {{ App\Models\Patient::distinct('patientnumber')->count('patientnumber') ?? 0 }}
                         </span>
                     </a>
                 </li>
